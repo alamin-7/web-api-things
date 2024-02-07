f1e8133e2b3db4ede7f131028fe35c99b6bfb8e3
alamin-7 <alamin188914@gmail.com>
2024-02-07 19:34:48 +0600

    json.py added

---
+++ json.py
@@ -0,0 +1,4 @@
+# json.load() is a function provided by the json module that is used to parse 
+# JSON data from a file-like object or a string and convert it into a Python object.
+
+# json.load() returns a Python object representing the JSON data.
